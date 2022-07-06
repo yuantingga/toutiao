97a651d96f8a53296fa20c2d998ea870a7794867
yuanttrerfd <1598538014@qq.com>
2022-07-06 13:33:45 +0800

    优化刷新404

--- src/router/index.js
+++ src/router/index.js
@@ -101,8 +101,6 @@ VueRouter.prototype.push = function push (location, onResolve, onReject) {
   return originalPush.call(this, location).catch(err => err)
 }
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
